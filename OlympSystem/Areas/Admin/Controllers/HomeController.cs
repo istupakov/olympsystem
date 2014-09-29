e57e0d8578d81baf08fc09d4e60ee69698de6c5e
Ilya Stupakov <istupakov@gmail.com>
2014-09-29 12:50:31 +0700

    Add admin area

---
+++ OlympSystem/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OlympSystem.Areas.Admin.Controllers
+{
+    [RouteArea("admin")]    
+    public class HomeController : Controller
+    {
+        // GET: /Admin/
+        [Route]
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ OlympSystem/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OlympSystem.Models;
+using OlympSystem.Controllers;
+
+namespace OlympSystem.Areas.Admin.Controllers
+{
+    [RouteArea("admin")]
+    [RoutePrefix("news")]
+    [Route("{action}/{id?}")]
+    public class NewsController : OlympBaseController
+    {
+        // GET: /Admin/News/
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.News.ToListAsync());
+        }
+
+        // GET: /Admin/News/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = await db.News.FindAsync(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        // GET: /Admin/News/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Admin/News/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include="NewsId,Title,Text,PublicationDate")] News news)
+        {
+            if (ModelState.IsValid)
+            {
+                db.News.Add(news);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(news);
+        }
+
+        // GET: /Admin/News/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = await db.News.FindAsync(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        // POST: /Admin/News/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include="NewsId,Title,Text,PublicationDate")] News news)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(news).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(news);
+        }
+
+        // GET: /Admin/News/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = await db.News.FindAsync(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        // POST: /Admin/News/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            News news = await db.News.FindAsync(id);
+            db.News.Remove(news);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
