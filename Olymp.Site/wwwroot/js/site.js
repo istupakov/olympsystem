6187336e9f52a38bfe43ab775a3f73fb9bbbe20b
Ilya Stupakov <istupakov@gmail.com>
2024-12-02 14:57:43 +0700

    Set color scheme from user prefers-color-scheme

--- Olymp.Site/wwwroot/js/site.js
+++ Olymp.Site/wwwroot/js/site.js
@@ -1,4 +1,12 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+function updateTheme() {
+    const colorMode = window.matchMedia("(prefers-color-scheme: dark)").matches ?
+        "dark" :
+        "light";
+    document.querySelector("html").setAttribute("data-bs-theme", colorMode);
+}
+
+updateTheme();
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme);
